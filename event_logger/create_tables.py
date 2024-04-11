cd1097c5b7516c01c1e61199fd61227f6d73a101
zeamort <zeamort@gmail.com>
2024-04-11 00:29:30 -0700

    added sqlite table creation and drop files

---
+++ event_logger/create_tables.py
@@ -0,0 +1,20 @@
+import sqlite3
+import yaml
+
+# Load the app_conf.yaml configuration 
+with open('app_conf.yaml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+conn = sqlite3.connect(app_config['datastore']['filename'])
+
+c = conn.cursor()
+c.execute('''
+          CREATE TABLE Statistics
+          (`id` INTEGER PRIMARY KEY ASC, 
+           `message` TEXT NOT NULL,
+           `code` TEXT NOT NULL,
+           `date_created` VARCHAR(100) NOT NULL)
+          ''')
+
+conn.commit()
+conn.close()
